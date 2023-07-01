c467c083fe546449f9466efa2cfd5eae77af8f6f
Brian Gawalt <bgawalt@gmail.com>
2023-07-01 10:53:22 -0700

    Rich Textifier function implemented but cant test right now cuz bsky is hurting

--- readerbot_atp.py
+++ readerbot_atp.py
@@ -41,9 +41,12 @@ Dependencies needed:
 """
 
 
+import dataclasses
+import pprint
 import requests
 import sys
 
+from collections.abc import Sequence
 from datetime import datetime, timezone
 
 import posting_history
@@ -74,6 +77,82 @@ def get_auth_token_and_did(
     return (auth_token, did)
 
 
+@dataclasses.dataclass
+class RichTextLink:
+    url: str
+    byte_start: int
+    byte_end: int
+
+    def __post_init__(self):
+        if self.byte_end <= self.byte_start:
+            raise ValueError(
+                f"byte_end ({self.byte_end}) must be greater than "
+                f"byte_start ({self.byte_start})")
+    
+    def to_json(self):
+        # TODO uhhhh unclear how to type annotate this
+        return {
+            "index": {
+                "byteStart": self.byte_start,
+                "byteEnd": self.byte_end
+            },
+            "features": [
+                {            
+                    "$type": "app.bsky.richtext.facet#link",
+                    "uri": self.url
+                }
+            ]
+        }
+
+
+@dataclasses.dataclass
+class RichTextMessage:
+    message_text: str
+    links: Sequence[RichTextLink]
+
+    def to_json(self, timestamp_iso: str):
+        # TODO type annotation
+        return {
+            "$type": "app.bsky.feed.post",
+            "createdAt": timestamp_iso,
+            "text": self.message_text,
+            "facets": [link.to_json() for link in self.links]
+        }
+
+
+# Test run produced, July 1 2023:
+# {'uri': 'at://did:plc:43nhmzgiugf65gwskj3kpfj5/app.bsky.feed.post/3jzi3r7uirc2d',
+#  'cid': 'bafyreid4tnueqsgcg2meqq5cut4bf5vkyhqgdiwxl737coajkfzyo3wuke'}
+def enrich_message(message: str) -> RichTextMessage:
+    """Adds hyperlinks as rich text (#ReaderBot hashtag and the sheets URL)."""
+    if not message.startswith("#ReaderBot"):
+        raise ValueError(f"message must start with '#ReaderBot': {message}")
+    hashtag_link = RichTextLink(
+        # TODO: Make this URL depend on the actual ATProto host.
+        url="https://bsky.app/search?q=%23ReaderBot",
+        byte_start=0,
+        byte_end=len("#ReaderBot".encode("UTF-8"))
+    )
+    # TODO: Make shortlink a constant, or better yet, a config param
+    if not message.endswith("https://goo.gl/pEH6yP"):
+        raise ValueError(
+            f"message must end with 'https://goo.gl/pEH6yP': {message}")
+    # TODO: cmon what's the actual str method for chopping off K chars
+    msg_open_brace = message[:(-1 * len("https://goo.gl/pEH6yP"))] + "["
+    sheet_start = len(msg_open_brace.encode("UTF-8"))
+    msg_unclosed = msg_open_brace + "Brian's Reading List"
+    sheet_end = len(msg_unclosed.encode("UTF-8"))
+    sheet_link = RichTextLink(
+        url="https://goo.gl/pEH6yP",
+        byte_start=sheet_start,
+        byte_end=sheet_end
+    )
+    return RichTextMessage(
+        message_text=(msg_unclosed + "]"),
+        links=(hashtag_link, sheet_link)
+    )
+
+
 def main():
     user_cred_filename = sys.argv[1]
     db_filename = sys.argv[2]
@@ -90,6 +169,7 @@ def main():
         return
     print(next_post.to_tuple())
     if "test"  in sys.argv:
+        print("Found 'test' in cmd line arguments; exiting now.")
         return
 
     print("READERBOT_POSTING")
@@ -101,17 +181,13 @@ def main():
     auth_token, did = get_auth_token_and_did(
         host=host, username=username, password=pword)
 
-    timestamp = dtime_now.isoformat().replace('+00:00', 'Z')
+    timestamp_iso = dtime_now.isoformat().replace('+00:00', 'Z')
     headers = {"Authorization": f"Bearer {auth_token}"}
     post_params = {
         "collection": "app.bsky.feed.post",
         "$type": "app.bsky.feed.post",
         "repo": "{}".format(did),
-        "record": {
-            "$type": "app.bsky.feed.post",
-            "createdAt": timestamp,
-            "text": next_post.message
-        }
+        "record": enrich_message(next_post.message).to_json(timestamp_iso)
     }
     resp = requests.post(
         f"{host}/xrpc/com.atproto.repo.createRecord",
@@ -119,7 +195,7 @@ def main():
         headers=headers
     )
     print(resp.status_code)
-    print(resp)
+    print(pprint.pprint(resp.json()))
     if resp.status_code != 200:
         raise RuntimeError("Posting failed!! POST_FAIL")
     posting_history.save_update(next_post, db_filename)
@@ -127,3 +203,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
